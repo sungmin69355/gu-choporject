aba79f8aa1317459cc8f489b2ddb1a81ed783ddf
sungmin69355 <sungmin69355@naver.com>
2020-07-21 20:25:41 +0900

    login.html, signup.html add

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',myapp.views.home,name='home'),
     path('accounts/',include('accounts.urls')),
-]
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
