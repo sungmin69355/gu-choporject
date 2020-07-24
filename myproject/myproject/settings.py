dfeb8ffcd34410d61963300ae4689710c1ebfe5a
sungmin69355 <sungmin69355@naver.com>
2020-07-25 00:50:03 +0900

    home.html 수정, static add

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -121,15 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-EMAIL_HOST = 'smtp.gmail.com'
-# 메일을 호스트하는 서버
-EMAIL_PORT = '587'
-# gmail과의 통신하는 포트
-EMAIL_HOST_USER = 'sungmin69355@naver.com'
-# 발신할 이메일
-EMAIL_HOST_PASSWORD = 'tjdals0927'
-# 발신할 메일의 비밀번호
-EMAIL_USE_TLS = True
-# TLS 보안 방법
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-# 사이트와 관련한 자동응답을 받을 이메일 주소,'webmaster@localhost'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'myapp', "static"), # myapp의 static 파일
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
